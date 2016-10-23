a4dbcda656a6562eba06741f519e46290f84ddc2
German Arcila <germanarcila@gmail.com>
2016-10-23 19:47:16 -0400

    all routes working, not styled

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express               = require('express'),
     mongoose              = require('mongoose'),
-    expressSanitizer      = require('express-sanitizer'),
     bodyParser            = require('body-parser'),
+    expressSanitizer      = require('express-sanitizer'),
+    methodOverride        = require('method-override'),
     app                   = express();
 
 mongoose.connect('mongodb://localhost/book_app');
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
+app.use(methodOverride('_method'));
 
 var bookSchema = new mongoose.Schema({
   title:      String,
@@ -53,6 +56,51 @@ app.post('/books', function (req, res) {
     }
   });
 });
+
+//SHOW ROUTE
+app.get('/books/:id', function (req, res) {
+  Book.findById(req.params.id, function (err, foundBook) {
+    if(err){
+      console.log('we have an error');
+    } else {
+      res.render('show', {book: foundBook})
+    }
+  });
+});
+
+//EDIT ROUTE
+app.get('/books/:id/edit', function (req, res) {
+  Book.findById(req.params.id, function (err, foundBook) {
+    if(err){
+      console.log('error in the edit route');
+    } else {
+      res.render('edit', {book: foundBook})
+    }
+  });
+});
+
+//UPDATE ROUTE
+app.put('/books/:id', function (req, res) {
+  req.body.book.body = req.sanitize(req.body.book.body)
+  Book.findByIdAndUpdate(req.params.id, req.body.book, function (err, foundBook) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/books/' + req.params.id)
+    }
+  });
+});
+
+//DELETE ROUTE
+app.delete('/books/:id', function (req, res) {
+  Book.findByIdAndRemove(req.params.id, function (err) {
+    if(err){
+      console.log('zapato')
+    } else {
+      res.redirect('/books');
+    }
+  })
+})
 
 app.listen(3000, function () {
   console.log('Server for learnig app with Express and MongoDB running');
